f6c96889e8bbf5d867a19f8fb7ffa67617a0257a
minaz65 <minazebarjadan@gmail.com>
2015-03-16 20:20:21 -0400

    CMSJobCategory(Insert/Update/Delete)

--- NotreDameReBuildOfficial/Controllers/CMSHomeController.cs
+++ NotreDameReBuildOfficial/Controllers/CMSHomeController.cs
@@ -62,5 +62,117 @@ namespace notre_dame_rebuild.Controllers
             return View();
         }
 
+        // ------------------------------------- //
+        // ----- JobCategory ADMIN by Mina ----- //
+        // ------------------------------------- //
+
+        jobCategory JobCatObj = new jobCategory(); // creating an instance of jobCategory
+        
+        //Get all Job categories
+        public ActionResult JobCategory()
+        {
+            var Jobcat = JobCatObj.getJobCategory();
+            return View(Jobcat); 
+        }
+
+        //Get Job category by id
+        public ActionResult JobCategory_Details(int id)
+        {
+            var JobCat = JobCatObj.getJobCategoryByID(id);
+            if (JobCat == null) 
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(JobCat); 
+            }
+        }
+
+        public ActionResult Insert_JobCategory()
+        {
+            return View();
+        }
+        [HttpPost]// restirict an action method by only post requests 
+        public ActionResult Insert_JobCategory(Job_category jobCat)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    JobCatObj.commitInsert(jobCat); // if the state of the model is  valid the commitInsert function will be called and new value will be commited to database.
+                    return RedirectToAction("JobCategory"); //after inserting values in database, page will redirect to the index page and show all data
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Update_JobCategory(int id)
+        {
+            var JobCat = JobCatObj.getJobCategoryByID(id);
+            if (JobCat == null) 
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(JobCat); 
+            }
+
+        }
+
+        [HttpPost]// restirict an action method by only post requests
+        public ActionResult Update_JobCategory(int id, Job_category JobCat)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    JobCatObj.commitUpdate(id, JobCat.title); // get the new values and passed them to fields by these parameter
+                    return RedirectToAction("JobCategory_Details/" + id);// the page will redirect to the Job Category Information
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+
+        public ActionResult Delete_JobCategory(int id)
+        {
+            var JobCat = JobCatObj.getJobCategoryByID(id);
+            if (JobCat == null)
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(JobCat);
+            }
+        }
+
+        [HttpPost] // restirict an action method by only post requests
+        public ActionResult Delete_JobCategory(int id, Job_category Jobcat)
+        {
+            try
+            {
+                JobCatObj.commitDelete(id);
+                return RedirectToAction("JobCategory");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        public ActionResult NotFound()
+        {
+            return View();
+        }
     }
 }
---
+++ NotreDameReBuildOfficial/Models/jobPosting.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotreDameReBuildOfficial.Models
+{
+    public class jobPosting
+    {
+        //creating an instance of Linq object
+        ndLinqClassDataContext jobObj = new ndLinqClassDataContext();
+
+        public IQueryable<Job_posting> getJobs()
+        {
+            //Creating an anonymous variable with its value beging the instance of Linq object
+            var allJobs = jobObj.Job_postings.Select(x => x);
+            //return IQueryable Jobposting for data bound control to bind
+            return allJobs;
+        }
+
+        public Job_posting getJobByID(int _id)
+        {
+            var allJob = jobObj.Job_postings.SingleOrDefault(x => x.id == _id);
+            return allJob;
+        }
+
+        //creating an instance of job_posting table as parameter
+        public bool commitInsert(Job_posting jobPo)
+        {
+            //to ensure all data will be disposed of when finished
+            using (jobObj)
+            {
+
+                jobObj.Job_postings.InsertOnSubmit(jobPo); //using our model to set tabale columns to new values being passed and providing it to the insert command
+                jobObj.SubmitChanges();//commit insert to database
+                return true;
+            }
+        }
+
+        public bool commitUpdate(int _id, string _title, string _type, string _department, string _description, decimal _salary, DateTime _date, int _category_id)
+        {
+            using (jobObj)
+            {
+                var objUpdateJob = jobObj.Job_postings.Single(x => x.id == _id);
+                //setting table columns to new values being passed
+                objUpdateJob.title = _title;
+                objUpdateJob.type = _type;
+                objUpdateJob.department = _department;
+                objUpdateJob.description = _description;
+                objUpdateJob.salary = _salary;
+                objUpdateJob.posting_date = _date;
+                objUpdateJob.category_id = _category_id;
+              
+                jobObj.SubmitChanges(); //commit update to database
+                return true;
+            }
+        }
+
+        public bool commitDelete(int _id)
+        {
+            using (jobObj)
+            {
+                var objDeleteJob = jobObj.Job_postings.Single(x => x.id == _id);
+                jobObj.Job_postings.DeleteOnSubmit(objDeleteJob);  //delete command
+                jobObj.SubmitChanges(); //commit delete against database
+                return true;
+
+            }
+        }
+    }
+}
