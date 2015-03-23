6ef47fe1f8d98e07876414fcb23d6a87b6cfa088
minaz65 <minazebarjadan@gmail.com>
2015-03-23 12:14:27 -0400

    Mina F1

--- NotreDameReBuildOfficial/Controllers/HomeController.cs
+++ NotreDameReBuildOfficial/Controllers/HomeController.cs
@@ -32,66 +32,9 @@ namespace notre_dame_rebuild.Controllers
         }
 
 
-        // ---------------------------- //
-        // ----- DONATIONS by GEN ----- //
-        // ---------------------------- //
+        
 
-        //Instantiates an object of the donationClass
-        donationClass objDonate = new donationClass();
-
-        //Donation Form Page Method
-        public ActionResult Donate()
-        {
-            return View();
-        }
-
-        //Inserts donation into db
-        [HttpPost]
-        public ActionResult Donate(Donation donation)
-        {
-            if (ModelState.IsValid)
-            {
-                try
-                {
-                    objDonate.insertDonation(donation);
-                    return RedirectToAction("Index"); //On sucessful insert, return to Donate page
-                }
-                catch
-                {
-                    //Error handling, return to Donation view if something goes wrong
-                    return View();
-                }
-            }
-
-            return View();
-        }
-
-        // ---------------------------- //
-        // ----- JobPosting by Mina ----- //
-        // ---------------------------- //
-
-        jobPosting JobPosObj = new jobPosting(); // creating an instance of jobPosting class
-
-        //Get all Jobs
-        public ActionResult JobPosting()
-        {
-            var JobPost = JobPosObj.getJobs();
-            return View(JobPost);
-        }
-
-        //Get Job by id
-        public ActionResult JobPosting_Details(int id)
-        {
-            var JobPost = JobPosObj.getJobByID(id);
-            if (JobPost == null)
-            {
-                return View("Not Found");
-            }
-            else
-            {
-                return View(JobPost);
-            }
-        }
+        
     
     
     
