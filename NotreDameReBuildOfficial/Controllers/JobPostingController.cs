aa3b4d33aff23c773f3ddae84b378a1fb135be5c
minaz65 <minazebarjadan@gmail.com>
2015-03-23 23:00:01 -0400

    JobApplying

---
+++ NotreDameReBuildOfficial/Controllers/JobPostingController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.IO; //for uploading file path
+
+//Imported Namespaces
+using NotreDameReBuildOfficial.Models;
+
+namespace NotreDameReBuildOfficial.Controllers
+{
+    public class JobPostingController : Controller
+    {
+        //
+        // GET: /JobPosting/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        jobPosting JobPosObj = new jobPosting(); // creating an instance of jobPosting class
+
+        //Get all Jobs
+        public ActionResult JobPosting()
+        {
+            var JobPost = JobPosObj.getJobs();
+            return View(JobPost);
+        }
+
+        //Get Job by id
+        public ActionResult JobPosting_Details(int id)
+        {
+            var JobPost = JobPosObj.getJobByID(id);
+            if (JobPost == null)
+            {
+                return View("Not Found");
+            }
+            else
+            {
+                return View(JobPost);
+            }
+        }
+
+    }
+}
