fdda1ffe69ed4246c229eba2e1d5eb458283dcc0
LukaszJaro <LukaszJaro>
2015-04-01 19:32:26 -0400

    Rough pdf filter

---
+++ NotreDameReBuildOfficial/Controllers/CMS/cmsPdfFilterController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using NotreDameReBuildOfficial.Models;
+
+namespace NotreDameReBuildOfficial.Controllers.CMS
+{
+    public class cmsPdfFilterController : Controller
+    {
+        PdfFilterClass objPdf = new PdfFilterClass(); //created instance of PdfFilterClass 
+
+        public ActionResult Pdf() //method gets all pdf's using model and displays it on pdf view  
+        {
+            var pdf = objPdf.getPdfs(); //calls method getPdfs from model
+            return View(pdf);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var pdf = objPdf.getPdfByID(id);
+            if (pdf == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(pdf);
+            }
+        }
+
+        public ActionResult createPdf()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult createPdf(PDF_Filter pdf)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+
+                try
+                {
+                    pdf.Tstamp = DateTime.Now; //automatically inserts datetime when form submitted 
+                    objPdf.insertPdf(pdf);
+                    return RedirectToAction("Pdf");
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult Update(int id)
+        {
+            var pdf = objPdf.getPdfByID(id);
+            if (pdf == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(pdf);
+            }
+        }
+        [HttpPost]
+        public ActionResult Update(int id, PDF_Filter pdf)
+        {
+            if (ModelState.IsValid)
+            {
+
+                try
+                {
+
+                    pdf.Tstamp = DateTime.Now;
+                    objPdf.updatePdf(id, pdf.PdfTitle, pdf.PdfUrl, pdf.Category, pdf.Image, pdf.Tstamp, pdf.Descr, pdf.Lang, pdf.Department);
+                    return RedirectToAction("Details/" + id);
+                }
+                catch
+                {
+                    return View();
+                }
+            }
+            return View();
+        }
+        public ActionResult Delete(int id)
+        {
+            var pdf = objPdf.getPdfByID(id);
+            if (pdf == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(pdf);
+            }
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id, PDF_Filter pdf)
+        {
+            try
+            {
+                objPdf.DeletePdf(id); //access pdf class model method DeletePdf
+                return RedirectToAction("Pdf");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
---
+++ NotreDameReBuildOfficial/Models/PdfFilterValidation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Web.Mvc;
+
+namespace NotreDameReBuildOfficial.Models
+{
+    [MetadataType(typeof(PdfFilterValidation))]
+    public partial class PDF_Filter //DB table name
+    { 
+        
+    }
+    [Bind(Exclude="id")]
+    public class PdfFilterValidation
+    {
+        [DisplayName("Title*")]
+        [Required(ErrorMessage = "Please enter your first name")]
+        [StringLength(100)]
+        public string PdfTitle { get; set; }
+
+        [DisplayName("Url*")]
+        [Required(ErrorMessage = "Please enter your last name")]
+        [StringLength(100)]
+        public string PdfUrl { get; set; }
+
+        [DisplayName("Category*")]
+        [Required(ErrorMessage = "Please enter a phone number")]
+        //[RegularExpression("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-][A-Za-z]{2,4}", ErrorMessage = "Please enter a valid email")]
+        public string Category { get; set; }
+
+        [DisplayName("Image*")]
+        //[Required(ErrorMessage = "Please enter a valid email")]
+        //[RegularExpression("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-][A-Za-z]{2,4}", ErrorMessage = "Please enter a valid email")]
+        public string Image { get; set; }
+
+        [DisplayName("Description*")]
+        [Required(ErrorMessage = "Please enter your health card number")]
+        [StringLength(12)]
+        public string Descr { get; set; }
+
+        [DisplayName("Language*")]
+        [Required(ErrorMessage = "Please choose an date")]
+        public string Lang { get; set; }
+
+        [DisplayName("Department*")]
+        [Required(ErrorMessage = "Please choose a time")]
+        //[DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")] 
+        public string Department { get; set; }
+
+    } 
+}
