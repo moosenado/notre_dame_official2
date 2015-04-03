bc95afe71df6213801afb1e89aa51d617e1eaaa0
Elliot Grabish <egrabish@gmail.com>
2015-04-03 17:33:21 -0400

    gift shop - beginnings

---
+++ NotreDameReBuildOfficial/Controllers/GiftShopController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NotreDameReBuildOfficial.Controllers
+{
+    public class GiftShopController : Controller
+    {
+        //
+        // GET: /GiftShop/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ NotreDameReBuildOfficial/Models/giftShopClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotreDameReBuildOfficial.Models
+{
+    public class giftShopClass
+    {
+        ndLinqClassDataContext objGS = new ndLinqClassDataContext(); 
+
+    }
+}
