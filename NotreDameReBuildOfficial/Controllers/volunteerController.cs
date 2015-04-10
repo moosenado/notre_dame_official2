b99f0abcc846417907eb090932487622a0c6403b
akshaydesai <akshaydesai92@gmail.com>
2015-04-10 15:54:37 -0400

    Volunteer Apply for Opportunity

---
+++ NotreDameReBuildOfficial/Controllers/volunteerController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using NotreDameReBuildOfficial.Models;
+
+namespace NotreDameReBuildOfficial.Controllers
+{
+    public class volunteerController : Controller
+    {
+        //
+        // GET: /volunteer/
+
+        volunteerJobs objVol = new volunteerJobs();
+
+        public ActionResult Opportunities()
+        {
+            var job = objVol.getJobs();
+            return View(job);
+        }
+
+        public ActionResult oppInfo(int id)
+        {
+            var job = objVol.getJobByID(id);
+            if (job == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                return View(job);
+            }
+        }
+
+
+    }
+}
