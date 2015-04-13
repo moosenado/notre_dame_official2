4cd8e1f0c33f7087a5437403db27d0df9c2f23be
minaz65 <minazebarjadan@gmail.com>
2015-04-13 11:51:55 -0400

    another commit

---
+++ NotreDameReBuildOfficial/Controllers/AccountNotreDameController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using NotreDameReBuildOfficial.Models;
+using System.Web.Security;
+using NotreDameReBuildOfficial.Infrastructure;
+
+
+namespace NotreDameReBuildOfficial.Controllers
+{
+    public class AccountNotreDameController : Controller
+    {
+        //
+        // GET: /AccountNotreDame/
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(User Login)
+        {
+            userClass user = new userClass().login(Login.user_name, Login.password);
+            if (user != null)
+            {
+                //FormsAuthentication.SetAuthCookie(user.UserName, false);
+                Util.CurrentUser = user;
+                FormsAuthentication.RedirectFromLoginPage(user.user_name, false);
+                //return RedirectToAction("Index", "Home");
+            }
+
+
+            ViewBag.Message = "Invalid user name or password.";
+            return View();
+        }
+
+        public ActionResult LogOff()
+        {
+            FormsAuthentication.SignOut();
+            Util.CurrentUser = null;
+            return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult AccessDenied()
+        {
+            return View();
+        }
+
+    }
+}
