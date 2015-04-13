0ae4ac67a5c996a26b56350809bb90f7f30acd1b
minaz65 <minazebarjadan@gmail.com>
2015-04-13 12:19:12 -0400

    customeAuthorization

---
+++ NotreDameReBuildOfficial/Infrastructure/CustomAuthorizeAttribute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using System.Web.Mvc;
+using NotreDameReBuildOfficial.Models;
+using System.Web.Routing;
+
+namespace NotreDameReBuildOfficial.Infrastructure
+{
+    public class CustomAuthorizeAttribute: AuthorizeAttribute
+    {
+        private readonly string[] allowroles;
+        public CustomAuthorizeAttribute(params string[] roles)
+        {
+            this.allowroles = roles;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool authorized = false; //base.AuthorizeCore(httpContext);
+            //if (!authorized)
+            //{
+            //    return false;
+            //}
+            userClass db = new userClass();
+            if (Util.CurrentUser != null)
+            {
+                foreach (string role in allowroles)
+                {
+                    if (Util.CurrentUser.roleTitle.ToLower() == role.ToLower())
+                    {
+                        authorized = true;
+                        break;
+                    }
+                }
+            }
+            return authorized;
+        }
+
+        //public override void OnAuthorization(AuthorizationContext filterContext)
+        //{
+
+        //    base.OnAuthorization(filterContext);
+        //}
+
+        //protected override void HandleUnauthorizedRequest(AuthorizationContext filterContext)
+        //{
+        //    //base.HandleUnauthorizedRequest(filterContext);
+        //}
+    }
+}
