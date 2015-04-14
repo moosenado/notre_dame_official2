ba4fd3aa974e9dceab54dabdf4ef9ce2a0e5dc82
akshaydesai <akshaydesai92@gmail.com>
2015-04-14 17:24:05 -0400

    Commit for create new page

---
+++ NotreDameReBuildOfficial/Controllers/createPageController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using NotreDameReBuildOfficial.Models;
+
+namespace NotreDameReBuildOfficial.Controllers
+{
+    public class createPageController : Controller
+    {
+        //
+        // GET: /createPage/
+
+        createPageClass objPage = new createPageClass();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult pageList()
+        {
+            var nav = objPage.getallNav();
+            return View(nav);
+        }
+
+        public ActionResult pageInfo(int id)
+        {
+            var nav = objPage.getallNavByID(id);
+            if (nav == null)
+            {
+                return View("NotFound");
+            }
+            else
+            {
+                TempData["id"] = id;
+                return View(nav);
+            }
+        }
+
+        public ActionResult insertsubPage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult insertsubPage(subNavigation snav)
+        {
+            if (ModelState.IsValid)
+            {
+                var navid = (int)TempData["id"];
+                snav.NavID = navid;
+                snav.controller = "createPage";
+                snav.pageView = "page";
+                objPage.InsertPage(snav);
+                return RedirectToAction("pageList");
+            }
+
+            return View();
+        }
+
+    }
+}
---
+++ NotreDameReBuildOfficial/Models/createPageClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NotreDameReBuildOfficial.Models
+{
+    public class createPageClass
+    {
+        ndLinqClassDataContext objLinq = new ndLinqClassDataContext();
+
+        public IQueryable<navigation> getallNav()
+        {
+            var allNav = objLinq.navigations.Select(x => x);
+            return allNav;
+        }
+
+        public IQueryable<subNavigation> getallNavByID(int _id)
+        {
+            var allvol = (from x in objLinq.subNavigations
+                          where x.NavID == _id
+                          select x);
+            return allvol;
+        }
+
+        public bool InsertPage(subNavigation snav)
+        {
+            using (objLinq)
+            {
+                objLinq.subNavigations.InsertOnSubmit(snav);
+                objLinq.SubmitChanges();
+                return true;
+            }
+        }
+
+
+    }
+}
