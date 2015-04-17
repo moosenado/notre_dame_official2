1c21cc9974916455d3a63162186f8ab1fd5ff662
LukaszJaro <LukaszJaro>
2015-04-17 11:39:51 -0400

    search changes

--- NotreDameReBuildOfficial/Controllers/SendMailerController.cs
+++ NotreDameReBuildOfficial/Controllers/SendMailerController.cs
@@ -3,16 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using System.Net.Mail;
+using System.Net.Mail; //namespace required for mail
 
 namespace Mail.Controllers
 //namespace NotreDameReBuildOfficial.ViewModels
 {
     public class SendMailerController : Controller
     {
-        //
-        // GET: /SendMailer/
-
         public ActionResult Mail()
         {
             return View();
@@ -21,21 +18,21 @@ namespace Mail.Controllers
         [HttpPost]
         public ViewResult Mail(SendMail.Models.MailModel _objModelMail)
         {
-            if (ModelState.IsValid)
+            if (ModelState.IsValid) //if no problems then proceed to process email
             {
-                MailMessage mail = new MailMessage();
-                mail.To.Add(_objModelMail.To);
+                MailMessage mail = new MailMessage(); //class for sending email
+                mail.To.Add(_objModelMail.To);  
                 mail.From = new MailAddress(_objModelMail.From);
                 mail.Subject = _objModelMail.Subject;
                 string Body = _objModelMail.Body;
                 mail.Body = Body;
                 mail.IsBodyHtml = true;
                 SmtpClient smtp = new SmtpClient();
-                smtp.Host = "smtp-mail.outlook.com";
+                smtp.Host = "smtp-mail.outlook.com"; //used outlook.com smtp
                 smtp.Port = 587;
                 smtp.UseDefaultCredentials = false;
                 smtp.Credentials = new System.Net.NetworkCredential
-                    ("notredamehospital@outlook.com", "NotreDame1415"); // Enter seders User name and password  
+                ("notredamehospital@outlook.com", "NotreDame1415"); // Username/Password credentials 
                 smtp.EnableSsl = true;
                 smtp.Send(mail);
                 return View("Mail", _objModelMail);
@@ -47,3 +44,4 @@ namespace Mail.Controllers
         }
     }
 }
+//Source: http://www.c-sharpcorner.com/UploadFile/sourabh_mishra1/sending-an-e-mail-using-Asp-Net-mvc/
